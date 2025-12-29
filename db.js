3628fe52be97de1c76b6d5a9a1befde5c6aa82ac
jayakumaram <jaikumaramesh@gmail.com>
2025-12-29 09:35:49 +0400

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "Admin",
+  database: "daily_job_automotive",
 });
